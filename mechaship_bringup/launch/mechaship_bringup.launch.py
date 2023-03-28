bdbed00fc58d0b327730ed51cc5f92e4c952a636
dadayoung0 <dayoungync@gmail.com>
2023-03-28 13:27:30 +0900

    변경 : bringup launch 실행 순서 변경 및 함수 변경

--- mechaship_bringup/launch/mechaship_bringup.launch.py
+++ mechaship_bringup/launch/mechaship_bringup.launch.py
@@ -1,41 +1,48 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.substitutions import ThisLaunchFileDir
-
-
-from launch_ros.actions import Node, PushRosNamespace, LifecycleNode
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    # csi 카메라 드라이버
+    camera_driver_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("jetson_csi_camera_ros2_driver"),
+                "launch",
+                "jetson_csi_camera_ros2_driver.launch.py",
+            )
+        )
+    )
+
     # LiDAR 드라이버
     mechaship_lidar_parameter = LaunchConfiguration(
         "mechaship_lidar_parameter",
         default=os.path.join(
             get_package_share_directory("mechaship_bringup"),
-            "param/mechaship_lidar.yaml",
+            "param",
+            "mechaship_lidar.yaml",
         ),
     )
     lidar_driver_launch_arg = DeclareLaunchArgument(
         "mechaship_lidar_parameter", default_value=mechaship_lidar_parameter
     )
     lidar_driver_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([ThisLaunchFileDir(), "/mechaship_lidar.launch.py"]),
-        launch_arguments={"mechaship_lidar_parameter": mechaship_lidar_parameter}.items(),
-    )
-
-    # csi 카메라
-    camera_driver_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            get_package_share_directory("jetson_csi_camera_ros2_driver")
-            + "/launch/jetson_csi_camera_ros2_driver.launch.py"
-        )
+            os.path.join(
+                get_package_share_directory("mechaship_bringup"),
+                "launch",
+                "mechaship_lidar.launch.py",
+            )
+        ),
+        launch_arguments={
+            "mechaship_lidar_parameter": mechaship_lidar_parameter
+        }.items(),
     )
 
     # LiDAR TF2
@@ -56,11 +63,10 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            camera_driver_launch,
             lidar_driver_launch_arg,
             lidar_driver_launch,
             lidar_tf_node,
             micro_ros_node,
-            # camera_driver_node,
-            camera_driver_launch,
         ]
     )
