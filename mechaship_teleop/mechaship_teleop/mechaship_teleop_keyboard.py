e292651294110b11c1656b09ade1632c3e9fca8b
max cha <max@nizcorp.kr>
2025-01-07 13:34:34 +0900

    fix : support new fw

--- mechaship_teleop/mechaship_teleop/mechaship_teleop_keyboard.py
+++ mechaship_teleop/mechaship_teleop/mechaship_teleop_keyboard.py
@@ -1,56 +1,15 @@
-# Copyright (c) 2011, Willow Garage, Inc.
-# All rights reserved.
-#
-# Software License Agreement (BSD License 2.0)
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of {copyright_holder} nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-# Author: Bishop Pearson
-
 import os
 import select
 import sys
 
 import rclpy
-from mechaship_interfaces.srv import Key, ThrottlePercentage
-from rclpy.qos import QoSProfile
+from std_msgs.msg import Float32
 
-if os.name == "nt":
-    import msvcrt
-else:
-    import termios
-    import tty
+import termios
+import tty
 
-OMO_R1MINI_MAX_LIN_VEL = 0.50
-OMO_R1MINI_MAX_ANG_VEL = 5.70
-
-THROTTLE_STEP_SIZE = 5
-KEY_STEP_SIZE = 5
+THROTTLE_STEP_SIZE = 5.0
+KEY_STEP_SIZE = 5.0
 
 msg = """
 Control Your mechaship!
@@ -114,59 +73,56 @@ def constrain(input_vel, low_bound, high_bound):
     return input_vel
 
 
-def check_linear_limit_velocity(velocity):
-    return constrain(velocity, 0, 100)
+def check_thruster_limit(vel):
+    return constrain(vel, 0.0, 100.0)
 
 
-def check_angular_limit_velocity(velocity):
-    return constrain(velocity, 60, 120)
+def check_key_limit(vel):
+    return constrain(vel, 60.0, 120.0)
 
 
 def main():
-    settings = None
-    if os.name != "nt":
-        settings = termios.tcgetattr(sys.stdin)
+    settings = termios.tcgetattr(sys.stdin)
 
     rclpy.init()
 
-    qos = QoSProfile(depth=10)
     node = rclpy.create_node("teleop_keyboard")
-    throttle_client = node.create_client(
-        ThrottlePercentage, "actuators/throttle/set_percentage"
+    key_publisher = node.create_publisher(Float32, "actuator/key/degree", 10)
+    throttle_publisher = node.create_publisher(
+        Float32, "actuator/thruster/percentage", 10
     )
-    key_client = node.create_client(Key, "actuators/key/set")
 
     status = 0
-    target_throttle = 0
-    target_key = 90
+    target_throttle = 0.0
+    target_key = 90.0
 
     try:
         print(msg)
         while 1:
             key = get_key(settings)
             if key == "w":
-                target_throttle = check_linear_limit_velocity(
+                target_throttle = check_thruster_limit(
                     target_throttle + THROTTLE_STEP_SIZE
                 )
                 status = status + 1
                 print_vels(target_throttle, target_key)
             elif key == "x":
-                target_throttle = check_linear_limit_velocity(
+                target_throttle = check_thruster_limit(
                     target_throttle - THROTTLE_STEP_SIZE
                 )
                 status = status + 1
                 print_vels(target_throttle, target_key)
             elif key == "a":
-                target_key = check_angular_limit_velocity(target_key - KEY_STEP_SIZE)
+                target_key = check_key_limit(target_key - KEY_STEP_SIZE)
                 status = status + 1
                 print_vels(target_throttle, target_key)
             elif key == "d":
-                target_key = check_angular_limit_velocity(target_key + KEY_STEP_SIZE)
+                target_key = check_key_limit(target_key + KEY_STEP_SIZE)
                 status = status + 1
                 print_vels(target_throttle, target_key)
             elif key == " " or key == "s":
-                target_throttle = 0
-                target_key = 90
+                target_throttle = 0.0
+                target_key = 90.0
                 print_vels(target_throttle, target_key)
             else:
                 if key == "\x03":
@@ -177,30 +133,27 @@ def main():
                 print(msg)
                 status = 0
 
-            throttle_set = ThrottlePercentage.Request()
-            key_set = Key.Request()
+            _msg = Float32()
 
-            throttle_set.percentage = target_throttle
-            key_set.degree = target_key
+            _msg.data = target_key
+            key_publisher.publish(_msg)
 
-            throttle_client.call_async(throttle_set)
-            key_client.call_async(key_set)
+            _msg.data = target_throttle
+            throttle_publisher.publish(_msg)
 
     except Exception as e:
         print(e)
 
     finally:
-        throttle_set = ThrottlePercentage.Request()
-        key_set = Key.Request()
+        _msg = Float32()
 
-        throttle_set.percentage = 0
-        key_set.degree = 90
+        _msg.data = 90.0
+        key_publisher.publish(_msg)
 
-        throttle_client.call_async(throttle_set)
-        key_client.call_async(key_set)
+        _msg.data = 0.0
+        throttle_publisher.publish(_msg)
 
-        if os.name != "nt":
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
 if __name__ == "__main__":
