c543bb78b9a35cd9677187a9c1bf938fb48372bd
dadayoung0 <dadayoung09@gmail.com>
2025-05-20 10:12:13 +0900

    add : add mechaship_simulation

---
+++ mechaship_simulation/launch/gazebo.launch.py
@@ -0,0 +1,112 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # 시뮬레이션 시간(Gazebo) 사용 여부
+    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
+
+    # world 파일 경로
+    world_path = os.path.join(
+        get_package_share_directory("mechaship_simulation"),
+        "worlds",
+        "empty.world",
+    )
+
+    # 로봇 형상 정보 publish
+    robot_state_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("mechaship_description"),
+                "launch",
+                "mechaship_state_publisher.launch.py",
+            )
+        ),
+        launch_arguments={"use_sim_time": use_sim_time}.items(),
+    )
+
+    # Gazebo ROS
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py"
+            )
+        ),
+        launch_arguments={"gz_args": world_path, "on_exit_shutdown": "true"}.items(),
+    )
+
+    # Gazebo에 로봇 불러오기
+    spawn_entity_node = Node(
+        executable="create",
+        package="ros_gz_sim",
+        name="create",
+        namespace="",
+        parameters=[
+            {
+                "world": "empty",
+                "topic": "robot_description",
+                "name": "mechaship",
+                "x": 0.0,
+                "y": 0.0,
+                "z": 0.02,
+            }
+        ],
+        # debug
+        output="screen",
+        emulate_tty=True,
+    )
+
+    # Gazebo ROS 브릿지
+    gazebo_bridge_parameter = os.path.join(
+        get_package_share_directory("mechaship_simulation"),
+        "param",
+        "mechaship_bridge.yaml",
+    )
+    gazebo_bridge_node = Node(
+        executable="parameter_bridge",
+        package="ros_gz_bridge",
+        name="parameter_bridge",
+        namespace="",
+        parameters=[
+            {
+                "config_file": gazebo_bridge_parameter,
+                "qos_overrides./tf_static.publisher.durability": "transient_local",
+            }
+        ],
+        # debug
+        output="screen",
+        emulate_tty=True,
+    )
+
+    # Gazebo ROS 이미지 브릿지
+    gazebo_image_bridge_node = Node(
+        executable="image_bridge",
+        package="ros_gz_image",
+        name="image_bridge",
+        namespace="",
+        arguments=["/image_raw"],
+        # debug
+        output="screen",
+        emulate_tty=True,
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="true",
+                description="Use simulation (Gazebo) clock if true",
+            ),
+            robot_state_publisher,
+            gazebo_launch,
+            spawn_entity_node,
+            gazebo_bridge_node,
+            gazebo_image_bridge_node,
+        ]
+    )
